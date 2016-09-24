c36518cb45292b3dad6cedcf4a64a5af8d52f93f
BoolInt <atc_tyrann@hotmail.com>
2016-09-24 23:57:25 +0200

    x

--- build.bat
+++ build.bat
@@ -12,7 +12,7 @@ if not "%PackageVersion%" == "" (
 
 call %GitVersion% /updateassemblyinfo true
 
-"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Solution.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Bqpl.Exceptions.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 mkdir Build
-call %nuget% pack "Bqpl.Contracts\Bqpl.Contracts.csproj" -symbols -o Build -p Configuration=%config% %version%
+call %nuget% pack "Bqpl.Exceptions\Bqpl.Exceptions.csproj" -symbols -o Build -p Configuration=%config% %version%
