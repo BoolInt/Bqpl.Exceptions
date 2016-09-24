8be238347eed7784f5fd11469defdbc11356dd76
BoolInt <atc_tyrann@hotmail.com>
2016-09-25 01:02:01 +0200

    x

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
 
+set project=Bqpl.Exceptions
+
 set config=%1
 if "%config%" == "" (
    set config=Release
@@ -10,9 +12,21 @@ if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
 )
 
-call %GitVersion% /updateassemblyinfo true
+REM Restore
+call dotnet restore
+if not "%errorlevel%"=="0" goto failure
 
-"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Bqpl.Exceptions.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+REM Build
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" %project%.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+if not "%errorlevel%"=="0" goto failure
 
-mkdir Build
-call %nuget% pack "Bqpl.Exceptions\Bqpl.Exceptions.csproj" -symbols -o Build -p Configuration=%config% %version%
+REM Package
+mkdir %cd%\Artifacts
+call dotnet pack %project% --configuration %config% %version% --output Artifacts
+if not "%errorlevel%"=="0" goto failure
+
+:success
+exit 0
+
+:failure
+exit -1
